840e164fc8283edd5d98e6d7cf6b21388b61f744
lattepapa <whitelatte46@gmail.com>
2022-01-10 22:07:12 +0900

    Initial Commit

---
+++ ios_02_ScreenTransaction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_02_ScreenTransaction
+//
+//  Created by Siwoo Baek on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
